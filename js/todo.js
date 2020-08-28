0d7a3e4dbd11387a0c9b5eb85ab414f088f8f420
Rucha A Butala <ruchabutala97@gmail.com>
2020-08-29 01:38:36 +0530

    Strike-Through Commit

--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,5 @@
+var indexTask = 0;
+
 function addTask() {
 	var task = document.getElementById("task").value;
 	console.log(task);
@@ -11,12 +13,15 @@ function addTask() {
 		message.innerHTML = " ";
 		var targetElement = document.createElement("input");
 		targetElement.type = "checkbox";
-		targetElement.id = "tasks";
+		targetElement.id = "tasks" + indexTask;
+		targetElement.name = "tasksList";
 		targetElement.class = "custom-control-input";
 
 		var label = document.createElement("label");
-		label.setAttribute("for", "tasks");
+		label.setAttribute("for", "tasks" + indexTask);
+		label.id = "tasksLabel" + indexTask;
 		label.class = "custom-control-label";
+		label.class = "lead";
 
 		label.appendChild(document.createTextNode(task));
 
@@ -26,12 +31,46 @@ function addTask() {
 
 		var breakLine = document.createElement("br");
 		target.appendChild(breakLine);
+		indexTask++;
 	}
 
 	document.getElementById("task").value = "";
 }
 
-function clearTask() {
+function strikeThrough() {
+	let elements = document.getElementsByName("tasksList");
+	for (var i = 0; i < elements.length; i++) {
+		let target = document.getElementById("tasksLabel" + i);
+		if (elements[i].checked) {
+			target.style.textDecoration = "line-through";
+		} else {
+			target.style.textDecoration = "none";
+		}
+	}
+}
+
+setInterval(strikeThrough, 1000);
+
+// function clearSelTask() {
+// 	let elements = document.getElementsByName("tasksList");
+// 	for (var i = 0; i < elements.length; i++) {
+// 		let targetLabel = document.getElementById("tasksLabel" + i);
+// 		if (elements[i].checked) {
+// 			// targetLabel.parentNode.removeChild(targetLabel);
+// 			// elements[i].parentNode.removeChild(elements[i]);
+
+// 			let element = document.getElementById("tasks" + i);
+
+// 			// targetLabel.innerHTML = "";
+// 			// element.parentNode.removeChild();
+
+// 			elements[i].nextSibling.remove();
+// 			elements[i].remove();
+// 		}
+// 	}
+// }
+
+function clearAllTask() {
 	var list = document.getElementById("list");
 	list.innerHTML = "";
 }
